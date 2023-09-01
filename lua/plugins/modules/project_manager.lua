b951023170b697b4a31f092523b5765b299bc587
Ewan <ewanrobsonn@gmail.com>
2023-09-01 15:20:23 +1000

    SetProject user command doesn't use patterns

--- lua/plugins/modules/project_manager.lua
+++ lua/plugins/modules/project_manager.lua
@@ -2,6 +2,7 @@ return {
   "ahmedkhalf/project.nvim",
   depedencies = {
     "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
   },
   cmd = {
     "ProjectRoot",
@@ -9,10 +10,19 @@ return {
   config = function()
     require("project_nvim").setup({
       manual_mode = true,
+      patters = {},
     })
 
     local telescope = require("telescope")
     telescope.load_extension("projects")
+
+    vim.api.nvim_create_user_command("SetProject", function()
+      local file = io.open(vim.fn.stdpath("data") .. "/project_nvim/project_history", "a")
+      if file ~= nil then
+        file:write(vim.fn.getcwd())
+        file:close()
+      end
+    end, {})
 
     vim.api.nvim_create_user_command(
       "OpenProject",
