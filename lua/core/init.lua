1993312da64bffcbc442105eb4a0b1795f101c49
Ewan <ewanrobsonn@gmail.com>
2023-12-16 15:22:50 +1100

    removed that should be ignored

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,15 +1,12 @@
 local keymaps = require("core.keymaps")
 local opts = require("core.options")
 local utils = require("core.utils")
+utils.prequire("config")
 
 opts.load_opts()
 utils.load_icons()
 keymaps.load_keymaps()
 
-local usr_init = utils.prequire("config")
-if usr_init then
-  usr_init.on_startup()
-end
 
 local plugins = require("core.plugins")
 plugins.load_plugins()
