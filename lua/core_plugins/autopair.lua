25d1464d5e09381c9dd965e7d723cc4fdb1f1c4f
Ewan <ewanrobsonn@gmail.com>
2023-12-20 13:53:48 +1100

    generalized core plugin filenames

--- lua/core_plugins/autopair.lua
+++ lua/core_plugins/autopair.lua
@@ -1,9 +1,8 @@
 local M = {}
 
 M.plugin = {
-  "altermo/ultimate-autopair.nvim",
-  event = { "InsertEnter", "CmdlineEnter" },
-  branch = "v0.6", --recomended as each new version will have breaking changes
+  "windwp/nvim-autopairs",
+  event = { "InsertEnter" },
   opts = {}
 }
 
