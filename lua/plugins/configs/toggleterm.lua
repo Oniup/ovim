b865d244b17c1af7075160ddd3c651e647cc453b
Ewan <ewanrobsonn@gmail.com>
2024-01-21 15:20:47 +1100

    larger toggleterm size

--- lua/plugins/configs/toggleterm.lua
+++ lua/plugins/configs/toggleterm.lua
@@ -3,6 +3,13 @@ local M = {}
 local u = require("core.utils")
 
 M.opts = {
+    size = function(term)
+        if term.direction == "horizontal" then
+            return 20
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+        end
+    end,
     shade_terminals = false,
     direction = "horizontal",
     autochdir = true,
